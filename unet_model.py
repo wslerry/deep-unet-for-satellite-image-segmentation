839af7ab897fa75f0681ad2b3a4cb8ab8303a83f
Lerry William <ghostspell89@yahoo.com>
2021-12-12 01:09:45 +0800

    from keras.optimizers_v1 import Adam

--- unet_model.py
+++ unet_model.py
@@ -2,7 +2,7 @@
 
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout
-from keras.optimizers import Adam
+from keras.optimizers_v1 import Adam #edit to old keras
 from keras.utils import plot_model
 from keras import backend as K
 
